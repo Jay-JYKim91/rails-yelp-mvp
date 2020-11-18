772b38fa8516ea8580a1a4502d4862cdd0231df1
Juyeon Kim <jay.jykim91@gmail.com>
2020-11-18 16:06:35 +0900

    finished but the form is ugly

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :restaurants, only: [:index, :new, :create, :show] do
     resources :reviews, only: [:new, :create]
   end
 
-  # get "restaurants", to: "restaurants#index"
-  # get "restaurants/new", to: "restaurants#new"
-  # post "restaurants", to: "restaurants#create"
-  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
-
-  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
-  # post "restaurants/:restaurant_id/reviews", to: "reviews#create"
 end
